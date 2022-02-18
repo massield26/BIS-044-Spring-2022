4b7bab8b4a97f19e3dae6a275f900d0453040566
massield26 <98557946+massield26@users.noreply.github.com>
2022-02-17 19:17:59 -0500

    Add files via upload

---
+++ Assignment 02A.R
@@ -0,0 +1,4 @@
+AgeDays <- as.double(as.Date("2022-05-06") - Sys.Date())
+cat("Massiel, there are " ,AgeDays, "days left in the semester")
+
+
---
+++ Assignment 02B.R
@@ -0,0 +1,6 @@
+x <- 10:1
+y <- -4:5
+z <- c('Hockey', 'Football', 'Curling', 'Soccer', 'Rugby', 'Baseball', 'Golf', 'Basketball', 'Wrestling', 'Tennis')
+theDF <- data.frame(x,y,z)
+theDF
+sd(theDF$y)
